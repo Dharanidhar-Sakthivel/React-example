c3a321bbbef6e8c637b0638e3499be32a94ae100
Dharanidhar Gunasoundari Sakthivel <110248207+Dharanidhar-Sakthivel@users.noreply.github.com>
2022-08-14 20:03:17 +0100

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import HeadTitle from '../../common/HeadTitle/HeadTitle'
+import "./Home.css"
+
+const Hero = () => {
+  
+  return (
+
+    <>
+      
+    <section className='display'>
+       <img src="https://www.cityheart.co.uk/wp/wp-content/uploads/cityheart-gloucester-feat-1.jpg" alt=""></img>
+    </section>
+
+      
+    </>  
+  )
+}
+  
+
+export default Hero
