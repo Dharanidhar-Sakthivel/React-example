c7d32a739215409e1be5f6ca8221e69609e7bbcd
Dharanidhar Gunasoundari Sakthivel <110248207+Dharanidhar-Sakthivel@users.noreply.github.com>
2022-08-14 20:04:29 +0100

    Add files via upload

---
+++ AccommodationHome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AllItem from '../../Accommodation/AllItem'
+const AccommodationHome = () => {
+  return (
+    <>
+      <h1>AccommodationHome</h1>
+    </>
+  )
+}
+
+export default AccommodationHome
